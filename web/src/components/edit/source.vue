22d62ac83f4d2dfe4c544e68a2f26f28d8a67dac
Ookami-Git <83712144+Ookami-Git@users.noreply.github.com>
2025-05-27 13:29:58 +0200

    Editor : Add CSV to UI

--- web/src/components/edit/source.vue
+++ web/src/components/edit/source.vue
@@ -21,7 +21,7 @@ save.value.safe()
 
 //const support = ref(["file","url","execute","database"])
 const support = ref(["file","url","database","text"])
-const supportedFlat = ref(["json", "xml", "yml", "hcl", "text"])
+const supportedFlat = ref(["json", "yml", "csv", "xml", "hcl", "text"])
 const supportedDb = ref(["sqlite", "postgres", "mysql"])
 const JsonSource = ref({
                 "src": '',
