1421a47a1b9a875c7e75d2f7bf292166bee8394a
Ookami-Git <83712144+Ookami-Git@users.noreply.github.com>
2026-03-07 17:45:23 +0100

    SCSS centralisation

--- web/src/components/view.vue
+++ web/src/components/view.vue
@@ -8,10 +8,10 @@ import viewGrid from './view/viewGrid.vue';
 import viewRow from './view/viewRow.vue';
 
 const props = defineProps({
-  viewid: [String, Number],
-  viewStructure: Object,
-  viewItems: Object,
-  viewData: Object
+    viewid: [String, Number],
+    viewStructure: Object,
+    viewItems: Object,
+    viewData: Object
 });
 
 const route = useRoute();
@@ -28,83 +28,83 @@ const fetchError = ref(null);
 
 
 if (!props.viewStructure || !props.viewItems) {
-  // Mode normal : chargement API
-  const fetchViewStructure = async () => {
-    try {
-      const response = await axios.get(`${apiUrl}/view/` + route.params.viewid);
-      viewStructure.value = JSON.parse(response.data.parameters);
-    } catch (error) {
-      fetchError.value = error.response;
-      hasLoadError.value = true;
-      console.error('Error fetching view structure', error);
-    }
-  };
+    // Mode normal : chargement API
+    const fetchViewStructure = async () => {
+        try {
+            const response = await axios.get(`${apiUrl}/view/` + route.params.viewid);
+            viewStructure.value = JSON.parse(response.data.parameters);
+        } catch (error) {
+            fetchError.value = error.response;
+            hasLoadError.value = true;
+            console.error('Error fetching view structure', error);
+        }
+    };
 
-  const fetchViewItems = async () => {
-    axios.get(`${apiUrl}/view/${route.params.viewid}/items`)
-      .then(function (response) {
-        viewItems.value = response.data;
-      })
-      .catch(function (error) {
-        fetchError.value = error.response;
-        hasLoadError.value = true;
-        console.error(`Error fetching items for view ${route.params.viewid}`, error);
-      });
-  };
+    const fetchViewItems = async () => {
+        axios.get(`${apiUrl}/view/${route.params.viewid}/items`)
+            .then(function (response) {
+                viewItems.value = response.data;
+            })
+            .catch(function (error) {
+                fetchError.value = error.response;
+                hasLoadError.value = true;
+                console.error(`Error fetching items for view ${route.params.viewid}`, error);
+            });
+    };
 
-  const fetchViewData = async () => {
-    axios.get(`${apiUrl}/data/view/` + route.params.viewid, {
-      params: route.query
-    })
-      .then((response) => {
-        viewData.value = response.data;
-      })
-      .catch((error) => {
-        fetchError.value = error.response;
-        hasLoadError.value = true;
-        console.error('Error fetching view data', error);
-      });
-  };
+    const fetchViewData = async () => {
+        axios.get(`${apiUrl}/data/view/` + route.params.viewid, {
+            params: route.query
+        })
+            .then((response) => {
+                viewData.value = response.data;
+            })
+            .catch((error) => {
+                fetchError.value = error.response;
+                hasLoadError.value = true;
+                console.error('Error fetching view data', error);
+            });
+    };
 
-  watch(route, async () => {
-    hasLoadError.value = false;
-    viewStructure.value = null;
-    viewData.value = null;
-    viewItems.value = null;
-    await fetchViewStructure();
-    await fetchViewData();
-    await fetchViewItems();
-  }, { immediate: true });
+    watch(route, async () => {
+        hasLoadError.value = false;
+        viewStructure.value = null;
+        viewData.value = null;
+        viewItems.value = null;
+        await fetchViewStructure();
+        await fetchViewData();
+        await fetchViewItems();
+    }, { immediate: true });
 }
 </script>
 
 <template>
-  <template v-if="viewStructure && viewItems">
-    <viewGrid v-if="viewStructure.version === 2" :structure="viewStructure" :viewItems="viewItems" />
-    <viewRow v-else-if="viewStructure.version === 1" :structure="viewStructure" :viewItems="viewItems" />
-    <viewRow v-else :structure="{ version: 1, items: viewStructure }" :viewItems="viewItems" />
-  </template>
-  <div v-else-if="hasLoadError" class="row">
-    <div class="col-md-12">
-      <div class="card" aria-hidden="true"
-        style="position: fixed; top: 50%; left: 50%; transform: translate(-50%, -50%);">
-        <div class="card-header bg-danger">Error {{ fetchError.status }} - {{ fetchError.statusText }}</div>
-        <div class="card-body">
-          <h5 class="card-title placeholder-glow">
-            <span>Unable to load view: {{ route.params.viewid }}</span>
-          </h5>
-          <p class="card-text placeholder-glow">
-            <span class="placeholder col-7"></span>
-            <span class="placeholder col-4"></span>
-            <span class="placeholder col-4"></span>
-            <span class="placeholder col-6"></span>
-            <span class="placeholder col-8"></span>
-          </p>
+    <template v-if="viewStructure && viewItems">
+        <viewGrid v-if="viewStructure.version === 2" :structure="viewStructure" :viewItems="viewItems" />
+        <viewRow v-else-if="viewStructure.version === 1" :structure="viewStructure" :viewItems="viewItems" />
+        <viewRow v-else :structure="{ version: 1, items: viewStructure }" :viewItems="viewItems" />
+    </template>
+    <div v-else-if="hasLoadError" class="row">
+        <div class="col-md-12">
+            <div class="card" aria-hidden="true"
+                style="position: fixed; top: 50%; left: 50%; transform: translate(-50%, -50%);">
+                <div class="card-header bg-danger">Error {{ fetchError.status }} - {{ fetchError.statusText }}</div>
+                <div class="card-body">
+                    <h5 class="card-title placeholder-glow">
+                        <span>Unable to load view: {{ route.params.viewid }}</span>
+                    </h5>
+                    <p class="card-text placeholder-glow">
+                        <span class="placeholder col-7"></span>
+                        <span class="placeholder col-4"></span>
+                        <span class="placeholder col-4"></span>
+                        <span class="placeholder col-6"></span>
+                        <span class="placeholder col-8"></span>
+                    </p>
+                </div>
+            </div>
         </div>
-      </div>
     </div>
-  </div>
-  <loading v-else />
+    <loading v-else />
 </template>
 
 <style>
@@ -113,24 +113,24 @@ if (!props.viewStructure || !props.viewItems) {
 [data-bs-theme="dark"] .dtrg-level-0,
 [data-bs-theme="dark"] .dtrg-level-1,
 [data-bs-theme="dark"] .dtrg-level-2 {
-  background-color: #23272b !important;
-  color: #fff !important;
+    background-color: #23272b !important;
+    color: #fff !important;
 }
 
-.card-header {
-  position: sticky;
-  top: 0;
-  z-index: 10;
-  border-bottom: 1px solid var(--bs-border-color);
-}
+/* .card-header {
+    position: sticky;
+    top: 0;
+    z-index: 10;
+    border-bottom: 1px solid var(--bs-border-color);
+} */
 
-[data-bs-theme="dark"] .card-header {
-  background: var(--bs-dark);
+/* [data-bs-theme="dark"] .card-header {
+    background: var(--bs-dark);
 }
 
 [data-bs-theme="light"] .card-header {
-  background: var(--bs-light);
-}
+    background: var(--bs-light);
+} */
 </style>
 
 <style>
