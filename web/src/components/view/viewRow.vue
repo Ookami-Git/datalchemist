e300022cc018d51d30ad1bf7926191d3d6abc387
Ookami-Git <83712144+Ookami-Git@users.noreply.github.com>
2026-03-07 18:55:20 +0100

    Revert "Harmonisation grid & row"
    This reverts commit 61e4be0fe2500f16554562b0f0e42737b2a106f4.

--- web/src/components/view/viewRow.vue
+++ web/src/components/view/viewRow.vue
@@ -20,21 +20,22 @@ const dataNunjucks = inject('data');
 
 <template>
     <template v-for="(row, index) in structure.items" :key="index">
-        <div class="row view-layout-row g-3 mb-2">
-            <div v-for="(item, indexrow) in row" :key="indexrow" :class="[`col-md-${item.size}`, 'view-layout-col']">
-                <div v-if="dataNunjucks" class="card view-card h-100">
-                    <div v-if="item?.title" class="card-header view-card-header" v-html="item.title"></div>
-                    <div class="card-body view-card-body">
+        <div class="row">
+            <div v-for="(item, indexrow) in row" :key="indexrow" :class="`col-md-${item.size}`">
+                <div v-if="dataNunjucks" class="card">
+                    <div v-if="item?.title" class="card-header" v-html="item.title"></div>
+                    <div class="card-body">
                         <Item :data="dataNunjucks" :itemDescribe="viewItems[`i${item.itemid}`]" />
                     </div>
                 </div>
-                <div v-else class="card view-card h-100" aria-hidden="true">
-                    <div v-if="item.title" class="card-header view-card-header" v-html="item.title"></div>
-                    <div class="card-body view-card-body">
+                <div v-else class="card" aria-hidden="true">
+                    <div v-if="item.title" class="card-header" v-html="item.title"></div>
+                    <div class="card-body">
                         <placeHolder />
                     </div>
                 </div>
             </div>
         </div>
+        <br>
     </template>
 </template>
